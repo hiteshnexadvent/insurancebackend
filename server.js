2da7bcbf8467b2182fa40bdd70ac425ef2f7e8a5
hiteshnexadvent <hitesh@nexadvent.com>
2025-06-07 15:43:29 +0530

    Changes

--- server.js
+++ server.js
@@ -27,32 +27,32 @@ app.use(cors({
 
 // -------------------------------- session for live
 
-// app.use(session({
-//     secret: process.env.SECRET_SESSION_KEY,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         sameSite: 'none',
-//         secure: true,
-//         maxAge: 24 * 60 * 60 * 1000,
-//         httpOnly: true,
-//     }
-// }))
+app.use(session({
+    secret: process.env.SECRET_SESSION_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        sameSite: 'none',
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    }
+}))
 
 // -------------------------------- session for localhost
 
-app.use(
-    session({
-      secret: "hitesh2822", 
-      resave: false,
-      saveUninitialized: true,
-      cookie: { 
-        secure: false,
-        maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: true
-      }, 
-    })
-  );
+// app.use(
+//     session({
+//       secret: "hitesh2822", 
+//       resave: false,
+//       saveUninitialized: true,
+//       cookie: { 
+//         secure: false,
+//         maxAge: 24 * 60 * 60 * 1000,
+//         httpOnly: true
+//       }, 
+//     })
+//   );
 
 
 // ------------------------------------ mongoose
